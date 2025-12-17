14a902e5aceb5237da75aa7fe9ca26e2d25ed7bb
Clara <94222670+birkeara@users.noreply.github.com>
2025-12-17 08:29:45 +0100

    create pages chat and index

---
+++ app/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <uContainer class="page-container">
+    <h1 class="title">Nuxt Chat</h1>
+    <uButton to="/chat" class="button"> Start Chat </uButton>
+  </uContainer>
+</template>
+
+<style scoped>
+.page-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+title {
+  font-size: 2.25rem;
+  font-weight: bold;
+}
+
+.botton {
+  margin-top: 1.5rem;
+}
+</style>
